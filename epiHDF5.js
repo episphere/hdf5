aa83feb8fd4e159b317418ca895bb3ac5c1346f0
Jonas Almeida <jalmeida@mathbiol.org>
2021-09-29 16:57:03 -0400

    pako included

--- epiHDF5.js
+++ epiHDF5.js
@@ -1,12 +1,9 @@
 console.log(`epiHDF5.js loaded ${Date()}`)
 
-
-
-
 epiHDF5 = async function(){
     // dependencies
-    await epiHDF5.require('hdf5')
-
+    await epiHDF5.require('pako','https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.4/pako.min.js')
+    await epiHDF5.require('hdf5','https://cdn.jsdelivr.net/gh/usnistgov/jsfive@master/dist/hdf5.js')
 }
 
 epiHDF5.loadScript = async function(url='https://cdn.jsdelivr.net/gh/usnistgov/jsfive@master/dist/hdf5.js'){
@@ -26,15 +23,51 @@ epiHDF5.require = async function(v,url){
     return window[v]
 }
 
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('foo');
-  }, 300);
-});
-
-
-
-
-if(typeof(hdf5)){
-
+epiHDF5.fetch = function(file_url='https://ndownloader.figshare.com/files/7024985',fun=console.log,filename='datafile.hdf5'){
+  console.log(`loading ${file_url}`)
+  fetch(file_url)
+  .then(function(response) { 
+    return response.arrayBuffer() 
+  })
+  .then(function(buffer) {
+    var f = new hdf5.File(buffer, filename);
+    // do something with f;
+    // let g = f.get('group');
+    // let d = f.get('group/dataset');
+    // let v = d.value;
+    // let a = d.attrs;
+    fun(f)
+  });
 }
+
+//example: epiHDF5.fetch('https://ndownloader.figshare.com/files/7024271',_=>{x=_;console.log('done')})
+
+epiHDF5.loadData = function(fun=console.log,id='datafile'){ // upload input
+  if(!document.getElementById(id)){
+    var file_input = document.createElement('input')
+    file_input.type='file'
+    document.body.appendChild(file_input)
+  }else{
+    var file_input = document.getElementById(id);
+  }
+  file_input.onchange = (event) => {
+    //var file = file_input.files[0]; // only one file allowed
+    var file = event.target.files[0]; // only one file allowed
+    let datafilename = file.name;
+    let reader = new FileReader();
+    reader.onloadend = function(evt) { 
+      let barr = evt.target.result;
+      var f = new hdf5.File(barr, datafilename);
+      // do something with f...
+      fun(f)
+    }
+    reader.readAsArrayBuffer(file);
+    //file_input.value = "";
+  }
+} 
+
+epiHDF5.help = function(){
+  window.open('https://github.com/usnistgov/jsfive')
+}
+
+epiHDF5()
